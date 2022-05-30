96d14efdebd8d3578daf3cdde106d1d4077451d2
Cody-Student <88352645+Cody-Student@users.noreply.github.com>
2022-05-30 11:42:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//All the variables:
+var a = 1
+var c = 123456783674215986
+var bc = 54654
+var Guacamole = "SEFRS"
+var Pizza = "Sophie"
+var Cheese = "Elizabeth"
+var Pineapple = "Foster"
+var EL_Fudges = "Ruewen"
+var Bangs_Boy = "Song"
+var Hunkyhair = "Sencen"
+var EVERYONE = "Initials :TDS, BAV, KGS/KCS/KS, DAD, MAR, WZE"
+var Guess_the_book_series = "Can you guess where all these names come from? Lemme give you a hint: It's from my favorite series in the world!"
+
+
+//All the "counsiling"
+console.log(a);
+console.log(c/bc);
+console.log(Guacamole);
+console.log(Pizza);
+console.log(Cheese);
+console.log(Pineapple);
+console.log(EL_Fudges);
+console.log(Bangs_Boy);
+console.log(Hunkyhair);
+console.log(EVERYONE);
+console.log(Guess_the_book_series);
+console.log(Pizza, EL_Fudges).concat();
